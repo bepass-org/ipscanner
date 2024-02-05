8927b7437d161ab3edd19493fd0f667d6fc65d91
uoosef <ghobadi@yekaan.ir>
2024-02-05 19:21:21 +0330

    add support for ipv6

--- example/cfscanner/main.go
+++ example/cfscanner/main.go
@@ -6,7 +6,7 @@ func main() {
 	// new scanner
 	scanner := ipscanner.NewScanner(
 		ipscanner.WithHTTPPing(),
-		ipscanner.WithUseIPv6(canConnectIPv6("[2001:4860:4860::8888]:80")),
+		ipscanner.WithUseIPv6(true),
 	)
 	go scanner.Run()
 	select {}
